6aea7435152984ab923b62ff5cc3a33585fa8d6e
Celine <github@celinechole.com>
2020-01-27 15:58:34 -0500

    routes done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Switch, Route, NavLink } from "react-router-dom";
 import "./App.css";
 import Home from "./Home";
 import BrowserRouter from "./BrowserRouter";
@@ -11,14 +12,45 @@ import FavoriteDessert from "./FavoriteDessert";
 function App() {
   return (
     <div className="App">
-      <nav className="nav"></nav>
-      <Home />
-      <BrowserRouter />
-      <LinkNavLink />
-      <SwitchRoute />
-      <Resources />
-      <RouteProps />
-      <FavoriteDessert />
+      <nav className="nav">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/browserrouter" activeClassName="nav-selected">
+          Browser Router
+        </NavLink>
+        <NavLink to="/switch-route" activeClassName="nav-selected">
+          Switch & Route
+        </NavLink>
+        <NavLink to="/link" activeClassName="nav-selected">
+          Link & NavLink
+        </NavLink>
+        <NavLink to="/route-props" activeClassName="nav-selected">
+          Route Props
+        </NavLink>
+        <NavLink to="/resources" activeClassName="nav-selected">
+          Resources
+        </NavLink>
+      </nav>
+      
+      <Switch>
+        <Route path="/browserrouter">
+          <BrowserRouter />
+        </Route>
+        <Route path="/link">
+          <LinkNavLink />
+        </Route>
+        <Route path="/switch-route">
+          <SwitchRoute />
+        </Route>
+        <Route path="/resources">
+          <Resources />
+        </Route>
+        <Route path="/route-props">
+          <RouteProps />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
